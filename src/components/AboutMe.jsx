359ae11c28eec201283c284cd896bf9585dd8a2f
manish465 <ms5978307@gmail.com>
2020-04-03 21:14:28 -0700

    adde link pages

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class AboutMe extends Component {
+    state = {};
+    render() {
+        return <h1>AboutMe</h1>;
+    }
+}
+
+export default AboutMe;
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class LandingPage extends Component {
+    state = {};
+    render() {
+        return <h1>LandingPage</h1>;
+    }
+}
+
+export default LandingPage;
