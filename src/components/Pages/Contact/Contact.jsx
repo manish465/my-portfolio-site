af67170e982f7bc1009f8588c857d0d08613c7cb
manish465 <ms5978307@gmail.com>
2020-04-06 13:33:13 -0700

    contact page

--- src/components/Pages/Contact/Contact.jsx
+++ src/components/Pages/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Cell } from "react-mdl";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 import "./Contact.css";
 
 class Contact extends Component {
@@ -8,8 +8,52 @@ class Contact extends Component {
         return (
             <div className="contact-body">
                 <Grid className="contact-grid">
-                    <Cell col={6}>Half page</Cell>
-                    <Cell col={6}>Half page</Cell>
+                    <Cell col={6}>
+                        <h2>Manish Sahu</h2>
+                        <img
+                            src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+                            alt="Avatar"
+                        />
+                        <p className="contact-about">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Repudiandae repellat consequatur sequi nemo,
+                            ut hic, ullam cupiditate dolores distinctio suscipit
+                            ipsa voluptates pariatur quas incidunt soluta eaque
+                            impedit, delectus labore.
+                        </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr />
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent
+                                        className="list"
+                                        icon="person"
+                                    >
+                                        Person 1
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent
+                                        className="list"
+                                        icon="person"
+                                    >
+                                        Person 2
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent
+                                        className="list"
+                                        icon="person"
+                                    >
+                                        Person 3
+                                    </ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
                 </Grid>
             </div>
         );
