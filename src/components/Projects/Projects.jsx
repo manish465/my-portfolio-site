d5d0e2da7a98e25a4f3c381e802d3c8bf3cb3437
manish465 <ms5978307@gmail.com>
2020-04-05 22:18:15 -0700

    path config

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Tabs, Tab, Grid, Cell } from "react-mdl";
 import "./Projects.css";
-import CardElement from "./CardElement";
+import CardElement from "./Card/CardElement";
 
 class Projects extends Component {
     state = {
