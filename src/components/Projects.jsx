38ed67b1b153904eb97d4fe355da093c6628352b
manish465 <ms5978307@gmail.com>
2020-04-05 20:29:28 -0700

    added tabs in projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
+import { Tabs, Tab } from "react-mdl";
 
 class Projects extends Component {
-    state = {};
+    state = {
+        activeTab: 0,
+    };
+
+    toggleCategories = () => {
+        if (this.state.activeTab === 0) {
+            return <h1>React</h1>;
+        }
+    };
+
     render() {
-        return <h1>Projects</h1>;
+        return (
+            <div className="category-tabs">
+                <Tabs
+                    activeTab={this.state.activeTab}
+                    onChange={(tabId) => this.setState({ activeTab: tabId })}
+                    ripple
+                >
+                    <Tab>React</Tab>
+                    <Tab>HTML</Tab>
+                    <Tab>Flutter</Tab>
+                    <Tab>JavaScript</Tab>
+                </Tabs>
+                <section className="projects-grid">
+                    {this.toggleCategories()}
+                </section>
+            </div>
+        );
     }
 }
 
